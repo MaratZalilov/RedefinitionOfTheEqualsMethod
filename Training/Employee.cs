0443aa7b18fb4ebed3c16e62a08521f996417c72
студент2 <2@DESKTOP-QEFI0G7>
2022-12-12 10:43:39 +0300

    Добавьте файлы проекта.

---
+++ Training/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training;
+
+public class Employee
+{
+    private string Name { get; set; }
+    private string SurName { get; set; }
+    private string _workPlace;
+    private int _age = 18;
+
+    public Employee(string name, string surName, string workPlace)
+    {
+        Name = name;
+        SurName = surName;
+        this._workPlace = workPlace;
+        
+    }
+    public Employee()
+    {
+
+    }
+
+    public override bool Equals(object? obj)
+    {
+        var employee = obj as Employee;
+        if (obj == null || employee == null)
+        {
+            return false;
+        }
+        else if (Name == employee.Name && SurName == employee.SurName && _workPlace == employee._workPlace)
+        {
+            return true;
+        }
+        return false;
+    }
+    public override int GetHashCode()
+    {
+        return _age.GetHashCode();
+    }
+}
---
+++ Training/Program.cs
@@ -0,0 +1,14 @@
+﻿using Training;
+
+Employee employee1 = new ("Иван","Петров","Завод");
+Employee employee2 = new ("Иван", "Петров", "Завод");
+
+//if(employee1 == employee2)
+//{
+//    check = true;  
+//}
+//else
+//{
+//    check = false;
+//}
+Console.WriteLine(employee1.Equals(employee2));
